29a3a09f3a5282dbb7b80c7b3d24f493b76e54d6
Tô Thành Thương <tothuongthanh@gmail.com>
2018-09-21 23:56:41 +0700

    add make as team member

--- common/models/app-user.js
+++ common/models/app-user.js
@@ -27,6 +27,12 @@ module.exports = function(Appuser) {
     next();
   });
 
+  Appuser.remoteMethod('makeAsAdmin', {
+    accepts: [{ arg: 'id', type: 'string' }],
+    returns: { arg: 'success', type: 'boolean' },
+    http: { path: '/make-as-admin', verb: 'post' },
+  });
+
   Appuser.makeAsAdmin = function(id, cb) {
     var Role = app.models.Role;
     var RoleMapping = app.models.RoleMapping;
@@ -34,26 +40,96 @@ module.exports = function(Appuser) {
     Appuser.findById(id, function(err, user) {
       if (err) return cb(err);
       // create the admin role
-      Role.create({
-        name: 'admin',
-      }, function(err, role) {
+      Role.find({ name: 'admin' }, function(err, res) {
         if (err) cb(err);
-  
-        // make bob an admin
-        role.principals.create({
-          principalType: RoleMapping.USER,
-          principalId: user.id,
-        }, function(err, principal) {
-          cb(err);
-        });
+        if (res.length > 0) {
+          // make user an admin
+          res[0].principals.create(
+            {
+              principalType: RoleMapping.USER,
+              principalId: user.id,
+            },
+            function(err, principal) {
+              cb(err);
+              cb(null, true);
+            }
+          );
+        } else {
+          Role.create(
+            {
+              name: 'admin',
+            },
+            function(err, role) {
+              if (err) cb(err);
+
+              // make user an admin
+              role.principals.create(
+                {
+                  principalType: RoleMapping.USER,
+                  principalId: user.id,
+                },
+                function(err, principal) {
+                  cb(err);
+                }
+              );
+            }
+          );
+          cb(null, true);
+        }
       });
-      cb(null, true);
     });
   };
 
-  Appuser.remoteMethod('makeAsAdmin', {
+  Appuser.remoteMethod('makeAsTeamMember', {
     accepts: [{ arg: 'id', type: 'string' }],
     returns: { arg: 'success', type: 'boolean' },
-    http: { path: '/mask-as-admin', verb: 'post' },
+    http: { path: '/make-as-team-member', verb: 'post' },
   });
+
+  Appuser.makeAsTeamMember = function(id, cb) {
+    var Role = app.models.Role;
+    var RoleMapping = app.models.RoleMapping;
+
+    Appuser.findById(id, function(err, user) {
+      if (err) return cb(err);
+      // create the admin role
+      Role.find({ name: 'teamMember' }, function(err, res) {
+        if (err) cb(err);
+        if (res.length > 0) {
+          // make user an admin
+          res[0].principals.create(
+            {
+              principalType: RoleMapping.USER,
+              principalId: user.id,
+            },
+            function(err, principal) {
+              cb(err);
+              cb(null, true);
+            }
+          );
+        } else {
+          Role.create(
+            {
+              name: 'teamMember',
+            },
+            function(err, role) {
+              if (err) cb(err);
+
+              // make user an admin
+              role.principals.create(
+                {
+                  principalType: RoleMapping.USER,
+                  principalId: user.id,
+                },
+                function(err, principal) {
+                  cb(err);
+                }
+              );
+            }
+          );
+          cb(null, true);
+        }
+      });
+    });
+  };
 };
